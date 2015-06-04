680fb8e13e01267df326a44013c6b80ed2f7cc8c
dariush abbasi <dariushem@yahoo.com>
2015-06-04 19:58:16 +0430

    lesson two

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname' : 'Dariush'} # fake user
+	posts = [
+		{
+			'author' : {'nickname' : 'dap'},
+			'body' : 'Beautiful day in Qazvin'
+		},
+		{
+			'author' : {'nickname' : 'floo'},
+			'body' : 'Breaking Bad was awesome!'
+		}
+	]
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts=posts)
