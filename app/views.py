9e565ab9360bfacdd65452e9f26573e986bf317b
dariush abbasi <dariushem@yahoo.com>
2015-06-07 17:52:34 +0430

    lesson seven

--- app/views.py
+++ app/views.py
@@ -16,10 +16,11 @@ def after_login(resp):
 		flash("Invalid login. Please try again.")
 		return redirect(url_for('login'))
 	user = User.query.filter_by(email= resp.email).first()
-	if user is  None :
+	if user is None:
 		nickname = resp.nickname
 		if nickname is None or nickname == "":
 			nickname = resp.email.split('@')[0]
+		nickname = User.make_unique_nickname(nickname)
 		user = User(nickname = nickname, email = resp.email)
 		db.session.add(user)
 		db.session.commit()
@@ -38,6 +39,14 @@ def before_request():
 		db.session.add(g.user)
 		db.session.commit()
 
+@app.errorhandler(404)
+def not_found_error(error):
+	return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	return render_template('500.html'),500
 
 
 @app.route('/')
@@ -102,7 +111,7 @@ def user(nickname):
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
-	form = EditForm()
+	form = EditForm(g.user.nickname)
 
 	if form.validate_on_submit():
 		g.user.nickname = form.nickname.data 
